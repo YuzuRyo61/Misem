516317c5f7ff7ed79661eadc421eade1eab40891
YuzuRyo61 <yuzuryo61@yuzulia.com>
2020-09-24 11:57:58 +0900

    WIP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DashboardComponent} from "./pages/dashboard/dashboard.component";
+import {InstanceComponent} from "./pages/instance/instance.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'instance', component: InstanceComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
