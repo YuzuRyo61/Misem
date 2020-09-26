fe865c9935fe5f877cb806d028ff5ba347fcc06d
CookieRamen <ark@arkjp.net>
2020-09-26 12:52:13 +0900

    Fix failed test.

--- src/app/pages/instance/instance.component.spec.ts
+++ src/app/pages/instance/instance.component.spec.ts
@@ -1,6 +1,19 @@
 import {waitForAsync, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {InstanceComponent} from './instance.component';
+import {CommonModule} from '@angular/common';
+import {InstanceRoutingModule} from './instance-routing.module';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {FlexLayoutModule, FlexModule} from '@angular/flex-layout';
+import {MatButtonModule} from '@angular/material/button';
+import {RouterTestingModule} from '@angular/router/testing';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 describe('InstanceComponent', () => {
   let component: InstanceComponent;
@@ -8,7 +21,24 @@ describe('InstanceComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [InstanceComponent]
+      declarations: [InstanceComponent],
+      imports: [
+        CommonModule,
+        RouterTestingModule,
+        InstanceRoutingModule,
+        BrowserAnimationsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        ReactiveFormsModule,
+        MatCheckboxModule,
+        MatCardModule,
+        MatIconModule,
+        MatDividerModule,
+        FlexModule,
+        MatButtonModule,
+        FlexLayoutModule,
+        FormsModule,
+      ]
     })
       .compileComponents();
   }));
