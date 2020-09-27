8a3415d8afa5016e337476df44217ebddfde3b32
YuzuRyo61 <yuzuryo61@yuzulia.com>
2020-09-27 16:30:59 +0900

    Add account service and miauth page implementing

--- src/app/pages/miauth/miauth.component.ts
+++ src/app/pages/miauth/miauth.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatDialog, MatDialogRef} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-miauth',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MiauthComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
 
+  logoutConfirm(): void {
+    const dialogLogout = this.dialog.open(MiauthComponentLogoutDialog, {
+      width: '450px'
+    });
+
+    dialogLogout.afterClosed().subscribe(result => {
+      if (result !== true) return;
+      // TODO: この中にログアウトするための処理を書く
+    })
+  }
+}
+
+@Component({
+  selector: 'app-miauth-logout-dialog',
+  templateUrl: './miauth-logout-dialog.component.html'
+})
+export class MiauthComponentLogoutDialog {
+  constructor(
+    public dialogLogout: MatDialogRef<MiauthComponentLogoutDialog>){
+  }
+
+  close(): void {
+    this.dialogLogout.close();
+  }
 }
--- src/app/pages/miauth/miauth.module.ts
+++ src/app/pages/miauth/miauth.module.ts
@@ -2,16 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MiauthRoutingModule } from './miauth-routing.module';
-import {MiauthComponent} from "./miauth.component";
+import {MiauthComponent, MiauthComponentLogoutDialog} from "./miauth.component";
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
+import {MatDialogModule} from "@angular/material/dialog";
 
 
 @NgModule({
   declarations: [
-    MiauthComponent
+    MiauthComponent,
+    MiauthComponentLogoutDialog
   ],
   imports: [
     CommonModule,
-    MiauthRoutingModule
+    MiauthRoutingModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDialogModule
   ]
 })
 export class MiauthModule { }
