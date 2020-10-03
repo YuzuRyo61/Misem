4fb2175e5014698e54758ececa711f72a257c8be
YuzuRyo61 <yuzuryo61@yuzulia.com>
2020-10-03 15:37:48 +0900

    Queue interface improve

--- src/app/pages/queue/queue.component.ts
+++ src/app/pages/queue/queue.component.ts
@@ -1,11 +1,27 @@
 import {Component, OnInit} from '@angular/core';
 
+// TODO: 仮置きインターフェース。移動予定
+export interface delayInstance {
+  domain: string;
+  delayCount: number
+}
+
+
 @Component({
   selector: 'app-queue',
   templateUrl: './queue.component.html',
   styleUrls: ['./queue.component.css']
 })
 export class QueueComponent implements OnInit {
+  displayedColumns: string[] = ['domain', 'delayCount'];
+  delayInboundInstance: delayInstance[] = [
+  ]
+  delayOutboundInstance: delayInstance[] = [
+    {
+      domain: 'example.com',
+      delayCount: 1
+    }
+  ]
 
   constructor() {
   }
--- src/app/pages/queue/queue.module.ts
+++ src/app/pages/queue/queue.module.ts
@@ -3,13 +3,27 @@ import {CommonModule} from '@angular/common';
 
 import {QueueRoutingModule} from './queue-routing.module';
 import {QueueComponent} from './queue.component';
+import {MatCardModule} from "@angular/material/card";
+import {FlexModule} from "@angular/flex-layout";
+import {MatListModule} from "@angular/material/list";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatTableModule} from "@angular/material/table";
+import {MatPaginatorModule} from "@angular/material/paginator";
 
 
 @NgModule({
   declarations: [QueueComponent],
   imports: [
     CommonModule,
-    QueueRoutingModule
+    QueueRoutingModule,
+    MatCardModule,
+    FlexModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule
   ]
 })
 export class QueueModule {
