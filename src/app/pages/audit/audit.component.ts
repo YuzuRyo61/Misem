fb7e458117632f40784b479c4e24ca0b255e34da
YuzuRyo61 <yuzuryo61@yuzulia.com>
2020-10-09 22:42:51 +0900

    Audit log mock

--- src/app/pages/audit/audit.component.ts
+++ src/app/pages/audit/audit.component.ts
@@ -1,5 +1,19 @@
 import {Component, OnInit} from '@angular/core';
 
+/*
+// Misskeyをリバース・エンジニアリングしたけど、10/09現在利用しているモデレーションログの種類はこいつら
+'updateMeta',
+'clearQueue',
+'vacuum',
+'chartResync',
+'silence',
+'unsilence',
+'addEmoji',
+'removeEmoji',
+'suspend',
+'unsuspend',
+ */
+
 @Component({
   selector: 'app-audit',
   templateUrl: './audit.component.html',
--- src/app/pages/audit/audit.module.ts
+++ src/app/pages/audit/audit.module.ts
@@ -3,13 +3,17 @@ import {CommonModule} from '@angular/common';
 
 import {AuditRoutingModule} from './audit-routing.module';
 import {AuditComponent} from './audit.component';
+import {MatIconModule} from "@angular/material/icon";
+import {MatExpansionModule} from "@angular/material/expansion";
 
 
 @NgModule({
   declarations: [AuditComponent],
   imports: [
     CommonModule,
-    AuditRoutingModule
+    AuditRoutingModule,
+    MatIconModule,
+    MatExpansionModule
   ]
 })
 export class AuditModule {
